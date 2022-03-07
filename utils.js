8991ef73ddb9a811eec9f046c39e1560e081df5c
Magnus Lilja <magnus@connoisseur.se>
2022-03-07 16:29:03 +0100

    Added about-html page

--- utils.js
+++ utils.js
@@ -1,16 +1,16 @@
-function includeHTMLModule() {
+function includeHTMLModule(module) {
     var z, i, elmnt, file, xhttp;
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
         elmnt = z[i];
-        file = elmnt.getAttribute("include-html-module");
+        file = elmnt.getAttribute(module);
         if (file) {
             xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4) {
                     if (this.status == 200) { elmnt.innerHTML = this.responseText; }
                     if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-                    elmnt.removeAttribute("include-html-module");
+                    elmnt.removeAttribute(module);
                     includeHTML();
                 }
             }
